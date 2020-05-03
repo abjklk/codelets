24af4d1f82a29a30dc0686852cfd2ec22c35c0d6
abjklk <abjklk@gmail.com>
2020-05-03 23:25:09 +0530

    res final

--- res.py
+++ res.py
@@ -1,10 +1,15 @@
 import requests
 
 # Change usn at the end of url below
-url = 'http://kletecheresults.contineo.in/index.php/component/report/?task=getReport&id=procard&usn=01FE17BCS000'
+usn = input('Enter your usn: Eg. 01FE1XBCSXXX\n')
+
+url = 'http://kletecheresults.contineo.in/index.php/component/report/?task=getReport&id=procard&usn='+usn.upper()
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'}
 a = requests.get(url,headers=headers)
 if a.status_code == 200:
 	f = open(url[-6:]+'.pdf','w+b')
 	f.write(a.content)
 	f.close()
+	print(f'File Saved as {url[-6:]}.pdf')
+else:
+	print('Something went wrong :(')
