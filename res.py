8c373e8e551bbde1b483ea8dbc5654ebb4f9c100
abjklk <abjklk@gmail.com>
2020-05-03 23:08:54 +0530

    added res

--- res.py
+++ res.py
@@ -1,6 +1,7 @@
 import requests
 
-url = 'http://kletecheresults.contineo.in/index.php/component/report/?task=getReport&id=procard&usn=01FE17BME044'
+# Change usn at the end of url below
+url = 'http://kletecheresults.contineo.in/index.php/component/report/?task=getReport&id=procard&usn=01FE17BCS000'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'}
 a = requests.get(url,headers=headers)
 if a.status_code == 200:
